4b72548ba83601e130cc8875c1c3e3bf8b4f725d
Corentin Caton <corentincaton@gmail.com>
2022-07-12 19:06:57 +0200

    Main responsive

---
+++ src/ItemsRow.js
@@ -0,0 +1,13 @@
+const ItemsRow = ({row, items, handleClick}) => {
+    return ( 
+        <div className={"row row-" + row}>
+            {items.map((item) => (
+                <div className={"row-item row-item-" + item.id} key={item.id} style={{width: "calc(" + item.width + " - 2px)"}} onClick={() => handleClick(item.link)}>
+                    <div className="row-item-container">{item.name}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+ 
+export default ItemsRow;
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,31 @@
+import ItemsRow from "./ItemsRow";
+
 function Main() {
 
-    let items = [
+    let firstRowItems = [
         {name: "Chef privé", link: "PrivateChef", id:1},
-        {name: "Consulting", link: "Consulting", id:2}
+        {name: "Consulting", link: "Consulting", id:2},
+        {name: "", link: "", width: "", id:3}
+    ];
+
+    let secondRowItems = [
+        {name: "", link: "", id:1},
+        {name: "Pop-ups", link: "PopUps", id:2},
+        {name: "", link: "", id:3}
+    ];
+
+    let thirdRowItems = [
+        {name: "", link: "", id:1},
+        {name: "Le menu du mois", link: "Menu", id:2},
+        {name: "", link: "", id:3},
+        {name: "À propos", link: "About", id:4},
+    ];
+
+    let fourthRowItems = [
+        {name: "Les alliés", link: "Partners", id:1},
+        {name: "", link: "", id:2},
+        {name: "Contact", link: "Contact", id:3},
+        {name: "", link: "", id:4},
     ];
 
     function handleClick(param) {
@@ -11,15 +34,10 @@ function Main() {
 
     return (  
         <div className="Main">
-            <div className="row row-1">
-                {items.map((item) => (
-                    <div className={"row-item row-item-" + item.id} key={item.id} onClick={() => handleClick(item.link)}>{item.name}</div>
-                ))}
-            </div>
-            <div className="row-item row-item-1"></div>
-            <div className="row row-2"></div>
-            <div className="row row-3"></div>
-            <div className="row row-4"></div>
+            <ItemsRow row={1} items={firstRowItems} handleClick={handleClick} />
+            <ItemsRow row={2} items={secondRowItems} handleClick={handleClick} />
+            <ItemsRow row={3} items={thirdRowItems} handleClick={handleClick} />
+            <ItemsRow row={4} items={fourthRowItems} handleClick={handleClick} />
         </div>
     );
 }
