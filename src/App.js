3b6bf4294ecdf8a0c1d0007e0c23650a38eb777e
Corentin Caton <corentincaton@gmail.com>
2022-07-14 17:16:12 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Main from './Main';
 import Navbar from './Navbar';
+import {BrowserRouter as  Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <div className="content">
         <Navbar />
         <Main />
       </div>
     </div>
+    </Router>
   );
 }
 
