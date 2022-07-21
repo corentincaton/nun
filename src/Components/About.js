2d2fc593f408b33da5d477d6ff2adfd8bd5bf62c
Corentin Caton <corentincaton@gmail.com>
2022-07-21 17:29:42 +0200

    Add About

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,29 @@
+import { useState } from "react";
+import Header from "./Header";
+import ImageContainer from "./ImageContainer";
+import image from "../Assets/Images/07.jpg";
+import Paragraph from "./Paragraph";
+
 function About() {
+
+    const [header, setHeader] = useState({text: "À propos", color: "#40312E"});
+    const [text, setText] = useState("Anthony Foret, a grandi au Pays-basque lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.");
+    const [youtubeLink, setYoutubeLink] = useState("https://www.youtube.com/embed/dexWs7urJtk");
+
     return (  
         <div className="About">
+            <Header header={header} />
+            <div className="video-wrapper">
+                        <iframe width="560" height="315" src={youtubeLink} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+            <div className="split-section">
+                <div className="left">
+                    <Paragraph text={text} />
+                </div>
+                <div className="right">
+                    <ImageContainer srcImage={image} />
+                </div>
+            </div>
         </div>
     );
 }
